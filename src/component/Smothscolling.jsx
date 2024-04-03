2a5736149e262024bd93bcd577f626a8e9daf616
pithwalahussain12 <53372033+pithwalahussain12@users.noreply.github.com>
2024-04-03 18:04:23 +0530

    Update Smothscolling.jsx

--- src/component/Smothscolling.jsx
+++ src/component/Smothscolling.jsx
@@ -94,37 +94,27 @@ const Scroll = () => {
       </div>
       {/* </div> */}
      
+
+
       <AnimatedCursor
-        color={cursorColor}
-        innerSize={16}
-        outerSize={8}
-        trailingSpeed={10}
-        outerAlpha={0.2}
-        innerScale={0.7}
-        outerScale={9}
+        color="255,255,255"
+        innerSize={14}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={1}
+        hasBlendMode={true}
         outerStyle={{
           mixBlendMode: 'exclusion'
         }}
-        clickables={['a', 'button']}
+        innerStyle={{
+          backgroundColor: 'var(--cursor-color)',
+          mixBlendMode: 'exclusion'
+        }}
+        data-fixed="true"
+        className="fixed" // Adding the class for fixed positioning
+
       />
-
-{/* <AnimatedCursor
-          color="255,255,255"
-          innerSize={8}
-          outerSize={35}
-          innerScale={1}
-          outerScale={2}
-          outerAlpha={1}
-          hasBlendMode={true}
-          outerStyle={{
-            mixBlendMode: 'exclusion'
-          }}
-          innerStyle={{
-            backgroundColor: 'var(--cursor-color)',
-            mixBlendMode: 'exclusion'
-          }}
-        /> */}
-
 
     </header>
 
